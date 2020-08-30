441e0f45e65578eacd5c3e7d68c2acf4e8efc6ea
Giovanni Donda <grdonda@gmail.com>
2020-08-29 22:21:14 -0300

    Atualizando o projeto

---
+++ docker/default/create_database_default.sql
@@ -0,0 +1,9 @@
+# Comando para a criação da base de dados default
+
+SET character_set_client = utf8;
+SET character_set_connection = utf8;
+SET character_set_results = utf8;
+SET collation_connection = utf8_general_ci;
+
+
+# comando para remover o sudo do root
